3e404c7af83d3c94e86871a1985577b383e2cd3c
OganKuba <kubog123@gmail.com>
2024-02-06 15:48:00 +0100

    final version

--- Backend_Officiely/src/main/java/com/example/backend_officiely/dtos/SearchParams.java
+++ Backend_Officiely/src/main/java/com/example/backend_officiely/dtos/SearchParams.java
@@ -10,10 +10,15 @@ public class SearchParams {
     private String startDateTime;
     private String endDateTime;
     private String sortByPrice;
-    private double minimumRating;
+    @Builder.Default
+    private double minimumRating=0;
     private boolean wifi;
-    private double minimumPrice;
-    private double maximumPrice;
-    private double minimumCapacity;
-    private double maximumCapacity;
+    @Builder.Default
+    private double minimumPrice = 0;
+    @Builder.Default
+    private double maximumPrice=100000000;
+    @Builder.Default
+    private double minimumCapacity=0;
+    @Builder.Default
+    private double maximumCapacity=100000000;
 }
